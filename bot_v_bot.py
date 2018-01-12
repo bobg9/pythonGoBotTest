3daedb23e9689ff9c4ebc9edf017d6919840a143
SagiN <medvidkov@gmail.com>
2018-01-12 08:05:35 +0200

    now with scoring!

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -6,6 +6,7 @@ from dlgo import gotypes
 from dlgo.agent.naive import RandomBot
 from dlgo.gamestate import GameState
 from dlgo.gui import GuiBoard
+from dlgo.scoring import compute_game_result
 from dlgo.utils import print_move, print_board
 
 
@@ -28,6 +29,9 @@ def main():
         bot_move = bots[game.next_player].select_move(game)
         print_move(game.next_player, bot_move)
         game = game.apply_move(game.next_player, bot_move)
+
+    print("winner is:", game.winner())
+    print("score is is:", compute_game_result(game))
     input("Press Enter to continue...")
 
 
